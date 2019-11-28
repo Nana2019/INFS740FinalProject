798e17e708f52a39c910cbe958a8598fa789a7ee
gliu123 <gliu123@umd.edu>
2019-11-28 16:31:11 -0500

    mutiple lines

--- datafind.php
+++ datafind.php
@@ -24,15 +24,33 @@
     $conn = null;
 ?>
 
-<?php
-foreach ($results as $temp) {
-  echo "<tr>";
-    echo "<td>".$temp['name']."</td> ";
-    echo "<td>".$temp['course']."</td> ";
-    echo "<td>".$temp['department']."</td> ";
-    echo "<td>".$temp['semester']."</td> ";
-    echo "<td>".$temp['rating']."</td> ";
-    echo "<td>".$temp['comments']."</td> ";
-  echo "</tr>";
-}
-?>
+<html>
+<head>
+  <title>data find</title>
+</head>
+<body>
+  <h1>TA List: </h1>
+    <table>
+      <tr>
+        <th>Name</th>
+        <th>Course</th>
+        <th>Department</th>
+        <th>Semester</th>
+        <th>Rating</th>
+        <th>Comments</th>
+      </tr>
+      <?php
+        foreach ($results as $temp){
+          echo "<tr>";
+          echo "<td>".$temp['name']."</td> ";
+          echo "<td>".$temp['course']."</td> ";
+          echo "<td>".$temp['department']."</td> ";
+          echo "<td>".$temp['semester']."</td> ";
+          echo "<td>".$temp['rating']."</td> ";
+          echo "<td>".$temp['comments']."</td> ";
+          echo "</tr>";
+        }
+        ?>
+    </table>
+</body>
+</html>
